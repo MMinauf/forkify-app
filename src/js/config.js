c9b0e6adfd49f7b8f5e2b217c6734d1f5f2f6c9d
MinaDev <minaufmario.og@gmail.com>
2022-09-09 10:49:04 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3e78ca44-65ea-49cf-a58c-5b4239dc2f1a';
+export const MODAL_CLOSE_SEC = 2.5;
